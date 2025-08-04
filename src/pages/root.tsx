00fbba53cf8a8d21f660d424ba5ccc058f29ec77
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-04 14:44:55 -0500

    added styling to page

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,14 +1,23 @@
 import { Outlet } from "react-router";
+import { Box } from "@mui/material";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function RootLayout() {
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       <Header />
-      <Outlet />
+      <Box sx={{ flexGrow: 1 }}>
+        <Outlet />
+      </Box>
       <Footer />
-    </>
+    </Box>
   );
 }
