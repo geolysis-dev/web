cf0f4a2ddc332891a4862fd46f1bd15f68848c27
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-04 14:45:18 -0500

    modified some paths

--- src/pages/paths.ts
+++ src/pages/paths.ts
@@ -1,9 +1,9 @@
 const PATHS = {
   HOME: "/",
   LEGAL: {
-    ROOT: "/legal",
-    TERMS_OF_USE: "/legal/terms-of-use",
-    PRIVACY_POLICY: "/legal/privacy-policy",
+    ROOT: "/excel/legal",
+    TERMS_OF_USE: "/excel/legal/terms-of-use",
+    PRIVACY_POLICY: "/excel/legal/privacy-policy",
   },
   CONTACT: "/contact",
 };
