05fd950a5ee077fc41f63baf036a20eff04575a2
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-05 15:00:07 -0500

    projects description

---
+++ src/pages/projects-description.ts
@@ -0,0 +1,26 @@
+export const PROJECTS = [
+  {
+    name: "Geolysis Python Pkg",
+    description:
+      "Open-source python package for geotechnical analysis and modeling.",
+    link: "https://github.com/patrickboateng/geolysis",
+  },
+  {
+    name: "Geolysis for Excel",
+    description:
+      "Microsoft Excel addin that provides geotechnical functions (available) and data entry capabilities (in development).",
+    link: "",
+  },
+  {
+    name: "Geolysis for Desktop",
+    description:
+      "Desktop application for graphically modeling geotechnical simulation.",
+    link: "",
+  },
+  {
+    name: "Geolysis AI",
+    description:
+      "Offers machine learning models that are trained using geotechnical data.",
+    link: "",
+  },
+];
