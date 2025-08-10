c4ceb7167ebafabcdd0ff8395dc4038e3c47d084
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-10 14:14:07 -0500

    added scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Fade from "@mui/material/Fade";
+import Fab from "@mui/material/Fab";
 
 import { createBrowserRouter, RouterProvider } from "react-router";
 
@@ -11,7 +15,8 @@ import TermsOfUsePage from "./pages/terms-of-use";
 import PrivacyPolicyPage from "./pages/privacy-policy";
 import HomePage from "./pages/home-page";
 import ContactPage from "./pages/contact-page";
-import PATHS from "./pages/paths";
+import PATHS from "./utils/paths";
+import Box from "@mui/material/Box";
 
 const theme = createTheme({
   colorSchemes: {
@@ -34,14 +39,54 @@ const router = createBrowserRouter([
   },
 ]);
 
+interface Props {
+  children?: React.ReactElement<unknown>;
+}
+
+function ScrollTop({ children }: Props) {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = () => {
+    const anchor = document.querySelector("#back-to-top-anchor");
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 16, right: 16, cursor: "pointer" }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
+
 function App() {
   return (
     <>
       <ThemeProvider theme={theme} defaultMode="dark" noSsr>
         <CssBaseline />
+        <div id="back-to-top-anchor" />
         <Container maxWidth="md">
           <RouterProvider router={router} />
         </Container>
+        <ScrollTop>
+          <Fab size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
       </ThemeProvider>
     </>
   );
