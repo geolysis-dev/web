7d12eaab2919bb2f596f4fcc74fd82a038bc8430
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-10 14:28:54 -0500

    SvgIcon receives props from logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,9 @@
 import SvgIcon from "@mui/icons-material/Menu";
 
+import type { SvgIconProps } from "@mui/material/SvgIcon";
+
 import logo from "../assets/logo.svg?react";
 
-export default function Logo() {
-  return (
-    <SvgIcon
-      component={logo}
-      sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
-      inheritViewBox
-    />
-  );
+export default function Logo({ ...props }: SvgIconProps) {
+  return <SvgIcon component={logo} {...props} inheritViewBox />;
 }
